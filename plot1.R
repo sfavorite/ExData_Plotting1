ef3119a4ab91bb4c2c804a9b205b6dc671c59217
Scot Favorite <scot.favorite@gmail.com>
2015-07-10 09:39:25 +0000

    Uploaded R code and corresponding plots as png files.

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("household_power_consumption.txt", sep=";", na.strings="?",  header=TRUE)
+subsetted <- filter(data, Date == "2/2/2007" | Date == "1/2/2007")
+subsetted$Date <- strptime(paste(subsetted$Date, subsetted$Time), format = "%d/%m/%Y %H:%M:%S")
+png("plot1.png")
+with(subsetted, hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)"))
+graphics.off()
