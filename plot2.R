d5468f825837a2b948a840621d6bd4ecaacb1723
Scot Favorite <scot.favorite@gmail.com>
2015-08-04 07:19:27 +0000

    Added library call for dplyr

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+library(dplyr)
 data <- read.table("household_power_consumption.txt", sep=";", na.strings="?",  header=TRUE)
 subsetted <- filter(data, Date == "2/2/2007" | Date == "1/2/2007")
 subsetted$Date <- strptime(paste(subsetted$Date, subsetted$Time), format = "%d/%m/%Y %H:%M:%S")
